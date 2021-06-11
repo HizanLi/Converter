f27b99285f3a8faa0472f91c5bfc568ff96c7c53
linxinl <linxinl@uvic.ca>
2021-06-11 10:35:07 +0800

    update ratio for length

---
+++ Version 1.1/Area.py
@@ -0,0 +1,300 @@
+# this is the ratio between kilometer and other length unit
+lengthKilometer = {"Kilometer": 1,
+               "Meter": 1000,
+               "Decimetre": 10000,
+               "Centimeter": 100000,
+               "Millimeter": 1000000,
+               "Nanometer": 1000000000000,
+               "Picometer": 1e+15,
+               "LightYear": 1.0570e-13,
+               "AstronomicalUnit.": 6.6846e-9,
+               "Inch": 39370.0787402,
+               "Foot": 3280.839895,
+               "Yard": 1093.6132983,
+               "Mile": 0.6213712,
+               "SeaMile": 0.539956,
+               "Furlong": 546.8066492,
+               "Fathom": 4.9709695
+               }
+
+# this is the ratio between meter and other length unit
+lengthMeter = {"Kilometer": 0.001,
+               "Meter": 1,
+               "Decimetre": 10,
+               "Centimeter": 100,
+               "Millimeter": 1000,
+               "Nanometer": 1000000000,
+               "Picometer": 1000000000000,
+               "LightYear": 1.057e-16,
+               "AstronomicalUnit.": 6.6846e-12,
+               "Inch": 39.3700787,
+               "Foot": 3.2908,
+               "Yard": 1.0936,
+               "Mile": 0.0006214,
+               "SeaMile": 0.00054,
+               "Furlong": 0.004971,
+               "Fathom": 0.5469
+               }
+
+# this is the ratio between Decimetre and other length unit
+lengthDecimetre = {"Kilometer": 0.0001,
+               "Meter": 0.1,
+               "Decimetre": 1,
+               "Centimeter": 10,
+               "Millimeter": 100,
+               "Nanometer": 100000000,
+               "Picometer": 100000000000,
+               "LightYear": 1.0570e-17,
+               "AstronomicalUnit.": 6.6846e-13,
+               "Inch": 3.9370079,
+               "Foot": 0.328084,
+               "Yard": 0.1093613,
+               "Mile": 0.0000621,
+               "SeaMile": 0.000054,
+               "Furlong": 0.0004971,
+               "Fathom": 0.0546807
+               }
+# this is the ratio between Centimeter and other length unit
+lengthCentimeter = {"Kilometer": 0.00001,
+               "Meter": 0.01,
+               "Decimetre": 0.1,
+               "Centimeter": 1,
+               "Millimeter": 10,
+               "Nanometer": 10000000,
+               "Picometer": 10000000000,
+               "LightYear": 1.0570e-18,
+               "AstronomicalUnit.": 6.6846e-14,
+               "Inch": 0.39370079,
+               "Foot": 0.0328084,
+               "Yard": 0.01093613,
+               "Mile": 0.00000621,
+               "SeaMile": 0.0000054,
+               "Furlong": 0.00004971,
+               "Fathom": 0.00546807
+               }
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
+
+# this is the ratio between Millimeter and other length unit
+lengthMillimeter = {"Kilometer": ,
+               "Meter": ,
+               "Decimetre": ,
+               "Centimeter": ,
+               "Millimeter": ,
+               "Nanometer": ,
+               "Picometer": ,
+               "LightYear": ,
+               "AstronomicalUnit.": ,
+               "Inch": ,
+               "Foot": ,
+               "Yard": ,
+               "Mile": ,
+               "SeaMile": ,
+               "Furlong": ,
+               "Fathom":
+               }
